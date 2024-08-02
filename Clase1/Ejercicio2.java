3a5e820dff214ab604a533223241835268127f82
Cristian Cappo <ccappopy@gmail.com>
2024-08-02 15:21:24 -0400

    Ajustado

--- Clase1/Ejercicio2.java
+++ Clase1/Ejercicio2.java
@@ -2,14 +2,18 @@
 *   Ejercicio 2.
 *   Leer una variable entera e imprimir su cuadrado
 */
+import java.util.Scanner;
 
 public class Ejercicio2 {
     public static void main(String [] args) {
         int n ;
 
         Scanner sc = new Scanner(System.in);
-        n = sc.readInt();
+        n = sc.nextInt();
 
-        System.out.printf("El cuadrado es %f\n", n*n);
+        System.out.printf("El cuadrado es %d\n", n*n);
+
+        sc.close();
+        
     }
 }
