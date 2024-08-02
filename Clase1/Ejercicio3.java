1fdceb1bc8f148e61478ff1b1f27c164242780c0
Cristian Cappo <ccappopy@gmail.com>
2024-08-02 15:31:51 -0400

    Ejercicio 3 - Clase 2

---
+++ Clase1/Ejercicio3.java
@@ -0,0 +1,25 @@
+/*
+*   Ejercicio 3.
+*   Leer la base y altura de un triángulo e imprimir su superficie
+*/
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String [] args) {
+        int base, altura ;
+        double superficie; 
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese el valor de la base:")
+        base = sc.nextInt();
+        System.out.print("Ingrese el valor de la altura:")
+        altura = sc.nextInt();
+        
+        superficie = (base * altura) / 2.0
+
+        System.out.printf("La superficie es %.3f\n", superficie);
+
+        sc.close();
+        
+    }
+}
