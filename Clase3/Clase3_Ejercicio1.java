22c0a00995a40d6dbaf7e7bff22835da9eff934b
Cristian Cappo <ccappopy@gmail.com>
2024-08-05 12:29:55 -0400

    Ajustar

--- Clase3/Clase3_Ejercicio1.java
+++ Clase3/Clase3_Ejercicio1.java
@@ -11,7 +11,7 @@
 import java.util.Scanner;
 
 public class Clase3_Ejercicio1 {
-    public static void main (String args ) {
+    public static void main (String [] args ) {
 
         Scanner sc = new Scanner(System.in);
 
