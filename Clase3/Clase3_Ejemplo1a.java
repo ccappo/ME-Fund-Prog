ab789476255ad3824108e2a70e5f18350f5729bc
Cristian Cappo <ccappopy@gmail.com>
2024-08-09 18:05:23 -0400

    Actualizar

--- Clase3/Clase3_Ejemplo1a.java
+++ Clase3/Clase3_Ejemplo1a.java
@@ -1,4 +1,6 @@
 /*
+ *  Maestria en Electrónica - Énfasis TIC
+ *  Fundamentos de Programación 2024
  * Ejemplo de uso de parámetros
  * Imprimir el cuadrado de un parámetro entero que se lee desde la linea de comandos
  */
